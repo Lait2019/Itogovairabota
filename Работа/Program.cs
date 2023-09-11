793d449738e6bf0ac677fbdb7d87b1eca70bca8d
Lait <mikhail.ryzhov.99@mail.ru>
2023-09-11 23:48:04 +0300

    3.add readme

--- Работа/Program.cs
+++ Работа/Program.cs
@@ -4,7 +4,7 @@ string [] array2 = new string [0];
 for (int i=0; i < n; i++)
 {
     if (array1[i].Length < 4)
-      {
+    {
         Array.Resize(ref array2, array2.Length + 1);
         array2[array2.Length - 1] = array1[i];
     }

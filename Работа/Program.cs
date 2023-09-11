da2c9a3bc1f4bb2195f2f79ac06d781656a98139
Lait <mikhail.ryzhov.99@mail.ru>
2023-09-11 23:30:55 +0300

    4.write programm

--- Работа/Program.cs
+++ Работа/Program.cs
@@ -1,2 +1,11 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿string [] array1 = { "Hello", "2", "world", ":-)"};
+int n = array1.Length;
+string [] array2 = new string [0];
+for (int i=0; i < n; i++)
+{
+    if (array1[i].Length < 4)
+      {
+        Array.Resize(ref array2, array2.Length + 1);
+        array2[array2.Length - 1] = array1[i];
+    }
+}
